0acb3bede0933761c03910c81fa7df7e22013cee
Naman Sharma <sharmanaman82@yahoo.co.in>
2024-07-15 19:33:40 +0530

    Update samplecode.py

--- lambda/samplecode.py
+++ lambda/samplecode.py
@@ -4,6 +4,6 @@ def lambda_handler(event, context):
     # TODO implement
     return {
         'statusCode': 200,
-        'body': json.dumps('Hello from Orange!!!')
+        'body': json.dumps('Hello World!!!')
     }
     
